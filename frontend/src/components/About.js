b52d974569732e10d7d554300fbda23b50d55690
Justas Drakšas <justas.draksas@mif.stud.vu.lt>
2024-09-22 22:29:51 +0300

    Create About,Home,Navbar components, add styling to navbar

---
+++ frontend/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h2>CHESS EXERCISER 900</h2>
+      <p>better than checkers</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>Labas krabas storas pyragas!</h1>
+        </div>
+    );
+  }
+
+  export default Home;
