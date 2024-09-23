e3b7dd456f83f8fd099f755846c38d72fcffcfd1
Justas Drakšas <justas.draksas@mif.stud.vu.lt>
2024-09-23 15:04:21 +0300

    enhance home view experience

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import '../styles/Home.css'
+import '../styles/Home.css';
 
 function Home() {
     return (
-        <div className="home">
-            <h1>Labas krabas storas pyragas!</h1>
+        <div className="home text-center">
+            <img
+                src={"/ce9000.png"}
+                alt="Chess Exerciser Logo"
+                className="home-logo img-fluid rounded-circle"
+            />
+              <audio autoPlay loop>
+                <source src="/bgmusic.mp3" type="audio/mpeg" />
+            </audio>
         </div>
     );
-  }
+}
 
-  export default Home;
-
-  
+export default Home;
