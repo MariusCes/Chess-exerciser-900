931dbdf23294bb1dc93426f91e9f29a6fea66125
Ignas Zuklys <ignas.zuklys@mif.stud.vu.lt>
2024-09-23 14:27:29 +0300

    added Stockfish 17 to the project

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d95f2295dc91d3dfd1274c6dd07f084c528200e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc3e4fb64660d56c5624804ed4720971163b7f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
