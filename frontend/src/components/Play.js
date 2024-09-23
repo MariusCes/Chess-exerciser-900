d213945974154c6a103f790e8214aed448b69e22
Dovydas Girskas <dovydas.girskas@mif.stud.vu.lt>
2024-09-24 02:03:26 +0300

    added basic api calls

--- frontend/src/components/Play.js
+++ frontend/src/components/Play.js
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
-import '../styles/Play.css';
+import React, { useState, useEffect } from "react";
+import "../styles/Play.css";
 
 function Play() {
-  const [move, setMove] = useState('');
+  const [move, setMove] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="play-screen">
-      <img src={"/chess.jpg"} alt="Chess board" className="chess-board-image" />
+      {/* <img src={"/chess.jpg"} alt="Chess board" className="chess-board-image" /> */}
+      {data.map((item) => (
+        <>
+          {item.title} {item.description}
+        </>
+      ))}
       <form className="move-form">
-        <input 
-          type="text" 
+        <input
+          type="text"
           value={move}
           onChange={(e) => setMove(e.target.value)}
           placeholder="B1 to B2"
