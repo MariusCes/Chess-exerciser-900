6a8edf34827799b89a06c8d4eb094c195f9395b8
Justas Drakšas <justas.draksas@mif.stud.vu.lt>
2024-09-23 11:28:09 +0300

    Add styling and content to About section, change navbar colors, remove unnecessary comments

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../styles/About.css';
 
 function About() {
   return (
-    <div className="about">
-      <h2>CHESS EXERCISER 900</h2>
-      <p>better than checkers</p>
+    <div className="about-section">
+      <h1>About Chess Exerciser</h1>
+      <p>
+        BNW Presents: Chess Exerciser 9000! (Now available on your personal computer!)
+        🚨 Attention all chess lovers! 🚨 Do you dream of becoming a Grandmaster? Tired
+        of forgetting those winning moves? Let BNW's Chess Exerciser 9000 sharpen your
+        mind and help you remember every move like never before!
+      </p>
+      <p>
+        Whether you're a beginner or an advanced player, Chess Exerciser is designed
+        to sharpen your skills, offering an exciting new twist on classic chess.
+      </p>
+      <img src={"/fruit.jpg"} alt="Fruit" className="about-image" />
     </div>
   );
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -10,3 +10,5 @@ function Home() {
   }
 
   export default Home;
+
+  
