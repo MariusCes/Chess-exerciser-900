afd1ec2a8284b54ba4399f4520d026697023a23e
MariusCes <marius.cesnauskas1@gmail.com>
2024-09-30 18:57:33 +0300

    improved Models and added create-game controller

--- backend/Models/Domain/Game.cs
+++ backend/Models/Domain/Game.cs
@@ -5,8 +5,7 @@ namespace backend.Models.Domain;
 public class Game
 {
     public Guid Id { get; set; }
-    public string[] MovesArray { get; set; }
-    public string UsersMove { get; set; }
+    public List<string>? MovesArray { get; set; }
     public int Lives { get; set; }
     public int Difficulty { get; set; }
     public int BotRating { get; set; }
