66bdcee10a6021c3205ef2a3dfe5f2a1d2afdba0
Dovydas Girskas <dovydas.girskas@mif.stud.vu.lt>
2024-10-09 20:38:48 +0300

    removed sound prank

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import '../styles/Home.css';
+import React from "react";
+import "../styles/Home.css";
 
 function Home() {
-    return (
-        <div className="home text-center">
-            <img
-                src={"/ce9000.png"}
-                alt="Chess Exerciser Logo"
-                className="home-logo img-fluid rounded-circle"
-            />
-              <audio autoPlay loop>
-                <source src="/bgmusic.mp3" type="audio/mpeg" />
-            </audio>
-        </div>
-    );
+  return (
+    <div className="home text-center">
+      <img
+        src={"/ce9000.png"}
+        alt="Chess Exerciser Logo"
+        className="home-logo img-fluid rounded-circle"
+      />
+    </div>
+  );
 }
 
 export default Home;
