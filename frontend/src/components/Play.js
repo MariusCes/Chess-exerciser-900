9940d3dac38ce445646bc85a841f6b13a8896a17
Justas Drakšas <justas.draksas@mif.stud.vu.lt>
2024-10-10 11:18:19 +0300

    create mock data for testing without back working

--- frontend/src/components/Play.js
+++ frontend/src/components/Play.js
@@ -8,37 +8,22 @@ function Play() {
   const [isGameCreated, setIsGameCreated] = useState(false); // jei nesukurtas zaidimas negali submittinti judesiu
   const [gameID, setGameID] = useState(""); // tas ID kuri atsiuncia
 
-  const createGame = async () => {
-    setMoveList([]);
-    const response = await fetch("http://localhost:5030/api/chess/create-game");
-    const data = await response.json(); // unboxing
-    setGameID(data.gameId);
+  const mockCreateGame = () => {
+    const mockGameId = "777777"; // Mock game ID
+    setGameID(mockGameId);
     setIsGameCreated(true);
+    setMoveList([]); // Reset move list on new game creation
+    console.log("Wow, the back has created a game ID without errors:", mockGameId);
   };
 
-  const postMove = async (userMove) => {
-    const response = await fetch(
-      "http://localhost:5030/api/chess/" + gameID + "/move",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          move: userMove,
-          id: gameID,
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      }
-    );
-
-    const data = await response.json();
-    if (data.wrongMove === false) {
-      setMoveList((prevMoves) => [...prevMoves, userMove, data.botMove]);
+  const mockPostMove = (userMove) => {
+    const mockBotMove = "E2E4"; // Mock bot move
+    if (userMove !== "bad") {
+      setMoveList((prevMoves) => [...prevMoves, userMove, mockBotMove]);
     } else {
       setMove("Bad move!");
     }
   };
-
   // atskiras component kuris butu uzloadinamas jei zaidimas sukurtas ir ten ir vyktu visas zaidimas, o siaip tai "create game" button ir vsio (checkai su "isGameCreated" butu)
 
   return (
@@ -47,7 +32,7 @@ function Play() {
         type="submit"
         onClick={(e) => {
           e.preventDefault();
-          createGame();
+          mockCreateGame();
         }}
       >
         Create Game
@@ -66,7 +51,7 @@ function Play() {
           type="submit"
           onClick={(e) => {
             e.preventDefault();
-            postMove(move);
+            mockPostMove(move);
             setMove("");
           }}
         >
