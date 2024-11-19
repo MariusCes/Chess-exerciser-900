8098f57c17a5b3b60c0ebf16e0f4f3195e04c507
MariusCes <marius.cesnauskas1@gmail.com>
2024-11-19 19:24:05 +0200

    change in obj

--- backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b9565a46013a0489f6264ce36ae52e60c1b1f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1eb33afd588df1e1ae841c98127f9969965bee4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
