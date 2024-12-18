71a1cdd3407dec144a777406d775551f375defc9
Ignas Zuklys <ignas.zuklys@mif.stud.vu.lt>
2024-12-18 22:46:04 +0200

    working blackout

--- frontend/src/components/History.js
+++ frontend/src/components/History.js
@@ -55,8 +55,8 @@ const History = () => {
                             className={`game-item ${game.wld === 1
                                 ? "game-won"
                                 : game.wld === 0
-                                    ? "game-lost"
-                                    : "game-draw"
+                                    ? "game-draw"
+                                    : "game-lost"
                                 }`}
                             onClick={() => toggleGameDetails(game.gameId, game.movesArraySerialized)}
                         >
@@ -65,15 +65,15 @@ const History = () => {
                                     className={`game-result ${game.wld === 1
                                         ? "game-won"
                                         : game.wld === 0
-                                            ? "game-lost"
-                                            : "game-draw"
+                                            ? "game-draw"
+                                            : "game-lost"
                                         }`}
                                 >
                                     {game.wld === 1
                                         ? "Victory"
                                         : game.wld === 0
-                                            ? "Defeat"
-                                            : "Draw"}
+                                            ? "Draw"
+                                            : "Defeat"}
                                 </span>
                             </div>
 
